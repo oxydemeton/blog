37b0d9ae228a47dc27ab710b23363aebaf8641aa
M B <mablasteam@gmail.com>
2023-03-18 22:18:06 +0100

    add nav bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,24 @@
 </script>
 
 <style lang="postcss">
-    
+    a {
+        @apply mx-1 p-1;
+    }
 </style>
 
-<header>
-    <h1>Heading</h1>
+<header class="mb-8 pb-4 border-b-4 shadow-xl grid grid-cols-1 xl:grid-cols-2 items-center">
+    <h1 class="text-2xl inline text-center">Mabla's Meinung</h1>
+    <nav class="flex align-middle justify-center flex-wrap-reverse shrink-0">
+        <a href="/archive">Archive</a>
+        <a href="/projects">Projects</a>
+        <form class="mx-2 p-1 rounded-md flex flex-nowrap
+            hover:outline outline-accent-light-sm
+                    dark:outline-accent-dark-sm"
+                rel="search" action="/search">
+            <input type="search" placeholder="search post"/>
+            <button type="submit" class="font-bold">Search</button>
+        </form>
+    </nav>
 </header>
 <slot />
 
