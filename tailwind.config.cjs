ba59f61d3120f579aed59bb63018a40f600de3f3
M B <mablasteam@gmail.com>
2023-03-18 21:39:04 +0100

    added theme colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,14 @@ module.exports = {
         '3xl': "var(--ft-size-3xl)",
         '4xl': "var(--ft-size-4xl)",
         '5xl': "var(--ft-size-5xl)"
+      },
+      colors: {
+        'main-dark': "var(--cs-main-dark)",
+        'main-light': "var(--cs-main-light)",
+        'accent-light-sm': "var(--cs-accent-light-sm)",
+        'accent-light-lg': "var(--cs-accent-light-lg)",
+        'accent-dark-sm': "var(--cs-accent-dark-sm)",
+        'accent-dark-lg': "var(--cs-accent-light-lg)"
       }
     },
   },
