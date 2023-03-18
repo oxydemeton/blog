a650e4febca293365e228f0833885d4da3f45880
M B <mablasteam@gmail.com>
2023-03-18 21:01:26 +0100

    added color scheme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,24 @@
 <script>
     import "../app.css";
+    import "../color-scheme.css";
 </script>
 
+<style lang="postcss">
+    :root {
+        --cs-cfg-main-dark: theme(colors.neutral.900);
+        --cs-cfg-main-light: theme(colors.neutral.100);
+        --cs-cfg-accent-dark-sm: theme(colors.orange.900);
+        --cs-cfg-accent-dark-lg: theme(colors.orange.700);
+        --cs-cfg-accent-light-sm: theme(colors.orange.100);
+        --cs-cfg-accent-light-lg: theme(colors.orange.400);
+    }
+</style>
+
+<header>
+    <h1>Heading</h1>
+</header>
 <slot />
+
+<footer>
+
+</footer>
