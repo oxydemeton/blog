b4b6dafaeebc8bfec0bb6cf75a8e5a56a5c24477
M B <mablasteam@gmail.com>
2023-03-18 21:26:08 +0100

    added font dynamic sizes to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        'sm': "var(--ft-size-sm)",
+        'base': "var(--ft-size-nm)",
+        'md': "var(--ft-size-md)",
+        'lg': "var(--ft-size-lg)",
+        'xl': "var(--ft-size-xl)",
+        '2xl': "var(--ft-size-2xl)",
+        '3xl': "var(--ft-size-3xl)",
+        '4xl': "var(--ft-size-4xl)",
+        '5xl': "var(--ft-size-5xl)"
+      }
+    },
   },
   plugins: [],
 }
