f5192de178356d652f04867ffeb5caa77bcaca68
M B <mablasteam@gmail.com>
2023-03-19 00:54:29 +0100

    add pocketbase

---
+++ src/lib/pocketbase.ts
@@ -0,0 +1,10 @@
+import PocketBase from "pocketbase"
+import { writable } from "svelte/store"
+
+export const pb = new PocketBase('http://88.99.125.155/pb');
+export const currentUser = writable(pb.authStore.model)
+pb.authStore.onChange(auth=> {
+    console.log("Auth change", auth);
+    
+    currentUser.set(pb.authStore.model) 
+})
