51aaa89782621a410b5786d68a432824107c0474
M B <mablasteam@gmail.com>
2023-03-18 22:49:47 +0100

    added search term to heading

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -4,5 +4,8 @@
 </script>
 
 <main>
-    <h2>Search Results</h2>
+    <h2>
+        Search Results for:
+        <q aria-label="Entered Search term" class="text-2xl font-medium">{data.search_term}</q>
+    </h2>
 </main>
