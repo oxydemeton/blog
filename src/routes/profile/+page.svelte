216fb2854d59048fe3e29f16c5d896a96a43aa7b
M B <mablasteam@gmail.com>
2023-03-19 20:00:32 +0100

    added email change

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import type { User } from "$lib/DbInterfaces";
+	import { readable } from "svelte/store";
     import {currentUser, pb} from "../../lib/pocketbase"
 	import Profile from "./Profile.svelte";
     let user = $currentUser as unknown as User;
@@ -7,6 +8,13 @@
     function sendVerify(){
         if (user.email) pb.collection('users').requestVerification(user.email);
     }
+    async function updateEmail() {
+        await pb.collection('users').requestEmailChange(newmail).catch((reason)=> {
+            newmail_msg = "An error occurred: " + reason
+        })
+    }
+    let newmail = ""
+    let newmail_msg = ""
 </script>
 
 <style lang="postcss">
@@ -33,6 +41,14 @@
                     <button on:click={sendVerify} class="btn-verify">Email senden </button>
                 </div>
             {/if}
+            <form on:submit={updateEmail}>
+                <fieldset>
+                    <legend class="text-lg">Update Email address</legend>
+                    <input type="email" name="newmail" placeholder="new email" bind:value={newmail} class="text-md">
+                    <button type="submit" class="text-md">Change Mail</button>
+                    <span class="block italic"> {newmail_msg}</span>
+                </fieldset>
+            </form>
         </article>
 
     {:else}
