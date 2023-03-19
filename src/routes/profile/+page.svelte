0b0d857ff458e079e6866c3584a89b59d8e85ac4
M B <mablasteam@gmail.com>
2023-03-19 14:59:55 +0100

    added page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import {currentUser, pb} from "../../lib/pocketbase"
+</script>
+
+<main>
+    {#if $currentUser}
+        <h2>{$currentUser.username}</h2>
+    {:else}
+    <h2>Not Logged in</h2>
+        <a href="/login" class="font-medium text-lg">Pleas login</a>
+    {/if}
+</main>
