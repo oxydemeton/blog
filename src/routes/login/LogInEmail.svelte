0649b1b51222e3505af5eeae77479a30a1db02b3
M B <mablasteam@gmail.com>
2023-03-19 14:37:52 +0100

    Added login with email

--- src/routes/login/LogInEmail.svelte
+++ src/routes/login/LogInEmail.svelte
@@ -1,10 +1,23 @@
 <script>
     import {currentUser, pb} from "../../lib/pocketbase"
     async function login() {
-        await pb.collection('users').authWithPassword(email, password)
+        pb.collection('users').authWithPassword(email, password)
+            .catch(()=>{
+                detail_message = "Login Failed. Pleas try again!"
+            }).then(()=> {
+                if(!pb.authStore.isValid) {
+                    pb.authStore.clear()
+                    detail_message = "Login Failed. Pleas try again!"
+                    console.debug("pb.authStore.isValid == false");
+                }else {
+                    password = ""
+                    email = ""
+                }
+            })
     }
     let email = ""
     let password = ""
+    let detail_message = ""
 </script>
 
 <form on:submit={login}>
@@ -13,5 +26,6 @@
         <input type="email" name="email" id="login-email-mail" placeholder="Email address" bind:value={email}>
         <input type="password" id="login-email-password" placeholder="Password" minlength="4" bind:value={password}>
         <input type="submit" value="Login">
+        <div>{detail_message}</div>
     </fieldset>
 </form>
