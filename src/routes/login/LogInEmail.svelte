1f992e5e25ec5119d54f052b9534bb3b583fe588
M B <mablasteam@gmail.com>
2023-03-19 14:23:56 +0100

    added email login

---
+++ src/routes/login/LogInEmail.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {currentUser, pb} from "../../lib/pocketbase"
+    async function login() {
+        await pb.collection('users').authWithPassword(email, password)
+    }
+    let email = ""
+    let password = ""
+</script>
+
+<form on:submit={login}>
+    <fieldset disabled={$currentUser != undefined}>
+        <legend>Email Login</legend>
+        <input type="email" name="email" id="login-email-mail" placeholder="Email address" bind:value={email}>
+        <input type="password" id="login-email-password" placeholder="Password" minlength="4" bind:value={password}>
+        <input type="submit" value="Login">
+    </fieldset>
+</form>
