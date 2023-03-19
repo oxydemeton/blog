ec9e30551adeef435fbe603dcccdc18e86f2589f
M B <mablasteam@gmail.com>
2023-03-19 15:07:07 +0100

    added labels

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,22 +19,23 @@
 </style>
 
 <header class="mb-8 pb-4 border-b-4 shadow-xl grid grid-cols-1 xl:grid-cols-2 items-center">
-    <h1 class="text-2xl inline text-center"><a class="no-underline" href="/">Mabla's Meinung</a></h1>
+    <h1 class="text-2xl inline text-center"><a class="no-underline" href="/" aria-label="Homepage">Mabla's Meinung</a></h1>
     <nav class="flex align-middle justify-center flex-wrap-reverse shrink-0">
         <a href="/archive">Archive</a>
         <a href="/projects">Projects</a>
         <form class="mx-2 p-1 rounded-md flex flex-nowrap
             hover:outline outline-accent-light-sm
                     dark:outline-accent-dark-sm"
-                rel="search" action="/search" method="get" on:submit={()=>refresh={}}>
+                rel="search" action="/search" method="get" on:submit={()=>refresh={}}
+                role="search">
             <input type="search" placeholder="search blog" name="search"/>
             <button type="submit" class="font-bold">Search</button>
         </form>
         {#if $currentUser}
-            <a href="/profile">{$currentUser.username}</a>
-            <button type="button" on:click={()=>pb.authStore.clear()} class="font-medium text-base">Logout</button>
+            <a href="/profile" aria-label="Profile">{$currentUser.username}</a>
+            <button type="button" on:click={()=>pb.authStore.clear()} aria-label="Logout" class="font-medium text-base">Logout</button>
         {:else}
-            <a href="/login">Login</a>
+            <a href="/login" aria-label="Login">Login</a>
         {/if}
     </nav>
     
