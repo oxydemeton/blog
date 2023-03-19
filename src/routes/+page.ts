4810d9a8c0c35a1120511277f96986d13e4d7edd
M B <mablasteam@gmail.com>
2023-03-19 18:42:52 +0100

    loading creator of latest post

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,8 +1,11 @@
-import {pb} from "../lib/pocketbase"
+import {pb} from "$lib/pocketbase"
+import {loadPostAuthor} from "$lib/DbUtil"
+import type { Post } from "$lib/DbInterfaces";
 export async function load() {
     const {items: [latest_post]} = await pb.collection('posts').getList(1, 1, {
         sort: '-created'
     })
+    latest_post.creatorExtend = await loadPostAuthor(latest_post as unknown as Post)
     console.log(latest_post);
     
     return {
