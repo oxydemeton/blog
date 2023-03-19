bbe17d190a0807ca2247481b6d3bf8d324230183
M B <mablasteam@gmail.com>
2023-03-19 19:34:00 +0100

    added user details

--- src/routes/profile/Profile.svelte
+++ src/routes/profile/Profile.svelte
@@ -4,5 +4,12 @@
 </script>
 
 <main>
-    <h2>{user.username}</h2>
+    <article>
+        <h2>{user.username}</h2>
+        <ul>
+            <li>Username: {user.username}</li>
+            <li>Creation: <time datetime={user.created}>{new Date(user.created).toLocaleDateString('de')}</time></li>
+            <li>Id: <span class="select-all italic font-extralight">{user.id}</span></li>
+        </ul>
+    </article>
 </main>
