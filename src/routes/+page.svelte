0be9e78d0037094cf024774c3eb253a22b4e699a
M B <mablasteam@gmail.com>
2023-03-19 18:22:15 +0100

    added latest post

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,24 @@
+<script lang="ts">
+	import type { Post } from "$lib/DbInterfaces";
+	import LatestPost from "./LatestPost.svelte";
+
+    export let data: {latest_post: Post};
+</script>
+<style lang="postcss">
+    .latest-post-sec {
+        border-bottom: .1rem dashed theme(colors.accent-light-sm);
+        border-top: .1rem dashed theme(colors.accent-light-sm);
+    }
+    @media (prefers-color-scheme: dark) {
+        .latest-post-sec {
+            border-bottom: .1rem dashed theme(colors.accent-dark-sm);
+            border-top: .1rem dashed theme(colors.accent-dark-sm);
+        }
+    }
+</style>
 <main>
-    <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dolores vero asperiores soluta commodi repellendus omnis sed facere eligendi autem! Dicta eos, quis dolorem quibusdam porro sit rem dolore nihil!
-        Eu nisi adipisicing cupidatat occaecat esse qui officia ea pariatur in eiusmod. Excepteur et reprehenderit reprehenderit laborum cupidatat aliquip. Excepteur nulla culpa Lorem ea tempor velit voluptate pariatur do ex proident. Et aute cupidatat minim elit aliqua ullamco pariatur id veniam. Aute ut cillum ullamco adipisicing cillum culpa nisi nostrud eiusmod ad dolor ipsum.
-    </p>
-    <p>
-        Ex Lorem enim velit sint mollit cupidatat minim. Incididunt culpa sunt tempor aute magna labore fugiat deserunt aliqua nulla et nostrud. Voluptate officia adipisicing enim duis veniam excepteur nostrud nisi. Cupidatat irure sit culpa veniam reprehenderit sunt cillum anim anim dolore dolore.
-    </p>
-    <p>
-        Culpa adipisicing ullamco voluptate ipsum dolor ut aliqua ut. Ut excepteur laboris sit voluptate consectetur incididunt consectetur aute ut do quis excepteur velit sunt. Dolor exercitation minim do excepteur do. Est sunt occaecat ea est.
-    </p>
+    <h2 class="mb-4">Latest Post</h2>
+    <section class="latest-post-sec md:px-24 max-h-[50vh] overflow-y-auto">
+        <LatestPost post={data.latest_post}/>
+    </section>
 </main>
