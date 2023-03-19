12429e355ce3d88ec1d04448def378e186b4cd14
M B <mablasteam@gmail.com>
2023-03-19 14:38:43 +0100

    added labels

--- src/routes/login/LogInEmail.svelte
+++ src/routes/login/LogInEmail.svelte
@@ -23,8 +23,8 @@
 <form on:submit={login}>
     <fieldset disabled={$currentUser != undefined}>
         <legend>Email Login</legend>
-        <input type="email" name="email" id="login-email-mail" placeholder="Email address" bind:value={email}>
-        <input type="password" id="login-email-password" placeholder="Password" minlength="4" bind:value={password}>
+        <input type="email" name="email" id="login-email-mail" placeholder="Email address" bind:value={email} aria-label="User Email address">
+        <input type="password" id="login-email-password" placeholder="Password" minlength="4" bind:value={password} aria-label="User Password">
         <input type="submit" value="Login">
         <div>{detail_message}</div>
     </fieldset>
