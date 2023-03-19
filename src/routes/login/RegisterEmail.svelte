85309ab7a5cd21c4d5b679458478d2d11fa6fc04
M B <mablasteam@gmail.com>
2023-03-19 14:48:04 +0100

    added email registration

--- src/routes/login/RegisterEmail.svelte
+++ src/routes/login/RegisterEmail.svelte
@@ -5,11 +5,28 @@
             detail_message = "Passwords don't match"
             return;
         }
-        await pb.collection('users').create({
-            name: username,
+        pb.collection('users').create({
+            username: username,
             email: email,
             password: password,
             passwordConfirm: passwordConfirm 
+        }).catch(()=>{
+                detail_message = "Registration Failed. Pleas try again!"
+            }).then(()=> {
+                pb.collection('users').authWithPassword(email, password)
+                .catch(()=>{
+                detail_message = "Login Failed. Pleas try again!"
+            }).then(()=> {
+                if(!pb.authStore.isValid) {
+                    pb.authStore.clear()
+                    detail_message = "Login Failed. Pleas try again!"
+                    console.debug("pb.authStore.isValid == false");
+                }else {
+                    detail_message = ""
+                    password = ""
+                    email = ""
+                }
+            })
         })
     }
     let username = ""
@@ -22,7 +39,7 @@
 <form on:submit={register}>
     <fieldset disabled={$currentUser != undefined}>
         <legend>Email Login</legend>
-        <input type="text" name="username" id="register-email-username" placeholder="Username" bind:value={username} aria-label="New Username">
+        <input minlength="3" type="text" name="username" id="register-email-username" placeholder="Username" bind:value={username} aria-label="New Username">
         <input type="email" name="email" id="register-email-mail" placeholder="Email address" bind:value={email} aria-label="New Email address">
         <input type="password" id="register-email-password" placeholder="Password" minlength="4" bind:value={password} aria-label="New Password">
         <input type="password" id="register-email-confim-password" placeholder="Confirm Password" minlength="4" bind:value={passwordConfirm} aria-label="Confirm Password">
