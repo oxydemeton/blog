b00b93c06993db3824623c68fd2e4e3b319b8aef
M B <mablasteam@gmail.com>
2023-03-19 20:23:11 +0100

    wrapped username and logout inside a div

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 
 <header class="mb-8 pb-4 border-b-4 shadow-xl grid grid-cols-1 xl:grid-cols-2 items-center">
     <h1 class="text-2xl inline text-center"><a class="no-underline" href="/" aria-label="Homepage">Mabla's Meinung</a></h1>
-    <nav class="flex align-middle justify-center flex-wrap-reverse shrink-0">
+    <nav class="flex align-middle justify-center flex-wrap shrink-0">
         <a href="/archive">Archive</a>
         <a href="/projects">Projects</a>
         <form class="mx-2 p-1 rounded-md flex flex-nowrap
@@ -32,8 +32,10 @@
             <button type="submit" class="font-bold">Search</button>
         </form>
         {#if $currentUser}
-            <a href="/profile" aria-label="Profile">{$currentUser.username}</a>
-            <button type="button" on:click={()=>pb.authStore.clear()} aria-label="Logout" class="font-medium text-base">Logout</button>
+            <div>
+                <a href="/profile" aria-label="Profile">{$currentUser.username}</a>
+                <button type="button" on:click={()=>pb.authStore.clear()} aria-label="Logout" class="font-medium text-base">Logout</button>
+            </div>
         {:else}
             <a href="/login" aria-label="Login">Login</a>
         {/if}
