e17fa2ad9738bbfee96ada3c57ba3cddae07944e
M B <mablasteam@gmail.com>
2023-03-19 18:55:05 +0100

    added 404 when user not found

--- src/routes/profile/[id]/+page.ts
+++ src/routes/profile/[id]/+page.ts
@@ -1,8 +1,13 @@
 import {pb} from "$lib/pocketbase"
 import {loadPostAuthor} from "$lib/DbUtil"
 import type { Post, User } from "$lib/DbInterfaces";
+import { error } from "@sveltejs/kit";
 export async function load({params}) {
-    const user = await pb.collection('users').getOne(params.id);
+    const user = await pb.collection('users').getOne(params.id).catch(()=> {
+        throw error(404, {
+            message: 'User not found'
+          });
+    });
     return {
         user: user as unknown as User
     }
