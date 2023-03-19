be79264e67e95a0a552540018d31517ff21961f8
M B <mablasteam@gmail.com>
2023-03-19 21:59:16 +0100

    uses https to fix mixed content errors

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase"
 import { writable } from "svelte/store"
 
-export const pb = new PocketBase('http://88.99.125.155/pb');
+export const pb = new PocketBase('https://88.99.125.155/pb');
 export const currentUser = writable(pb.authStore.model)
 pb.authStore.onChange(auth=> {    
     currentUser.set(pb.authStore.model) 
