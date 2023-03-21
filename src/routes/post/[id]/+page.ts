e0b73421166f4a7d1e678c9fae6720f15a82e426
M B <mablasteam@gmail.com>
2023-03-21 19:37:18 +0100

    added fetch of creator

--- src/routes/post/[id]/+page.ts
+++ src/routes/post/[id]/+page.ts
@@ -1,13 +1,14 @@
 import {pb} from "$lib/pocketbase"
-import {loadPostAuthor} from "$lib/DbUtil"
-import type { Post, User } from "$lib/DbInterfaces";
 import { error } from "@sveltejs/kit";
+import type { Post } from "$lib/DbInterfaces";
+
 export async function load({params}) {
-    const post = await pb.collection('posts').getOne(params.id).catch(()=> {
+    const post = await pb.collection('posts').getOne(params.id, {expand: 'creator'}).catch(()=> {
         throw error(404, {
             message: 'Post not found'
           });
     });
+    post.creatorExtend = post.expand.creator
     //post.creatorExtend = await loadPostAuthor(post as unknown as Post)
     return {
         post: post as unknown as Post
