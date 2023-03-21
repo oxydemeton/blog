efbad2dcd13adafb2b4c32a74dcdf3096552f879
M B <mablasteam@gmail.com>
2023-03-21 21:11:56 +0100

    improved design

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -6,23 +6,28 @@
 
     let comment_title = ""
     let comment_content = ""
+    let comment_notice = ""
     async function publishComment() {
         if($currentUser) {
-        const new_post = {
-            "title": comment_title,
-            "content": comment_content,
-            "creator": $currentUser.id,
-            "post": data.post.id
-        };
-        comment_content = ""
-        comment_title = ""
+            const new_post = {
+                "title": comment_title,
+                "content": comment_content,
+                "creator": $currentUser.id,
+                "post": data.post.id
+            };
 
-        const record = await pb.collection('comments').create(new_post);
-        if (data.post.expand['comments(post)'] && record) {
-            // @ts-ignore
-            data.post.expand['comments(post)'].push(record)
+            const record = await pb.collection('comments').create(new_post);
+            if (data.post.expand['comments(post)'] && record) {
+                // @ts-ignore
+                data.post.expand['comments(post)'].push(record)
+                comment_content = ""
+                comment_title = ""
+            } else {
+                comment_notice = "Irgendwas ist schief gelaufen beim erstellen."
+            }
+        }else {
+            comment_notice = "Du muss eingeloggt sein um einen Kommentar zu schreiben."
         }
-    }
     }
 </script>
 
@@ -38,16 +43,28 @@
     </article>
     <hr>
     <section class="comments">
+        <h2>Comments</h2>
         <form>
             <fieldset disabled={!$currentUser} aria-disabled={!$currentUser}>
-                <input type="text" aria-label="Title of new Comment" minlength="2" required placeholder="Titel" bind:value={comment_title}> <br>
-                <textarea name="" cols="30" rows="10" aria-label="Area to write a new Comments" placeholder="Kommentar" bind:value={comment_content}></textarea><br>
-                <button type="submit" on:submit={publishComment}>Kommentar veröffentlichen</button>
+                <legend>Write a new comment</legend>
+                {#if !$currentUser} 
+                    <i class="block">Logge dich ein um einen Kommentar zu verfassen.</i>
+                {/if}
+                <input class="m-1" type="text" aria-label="Title of new Comment" minlength="2" required placeholder="Titel" bind:value={comment_title}> <br>
+                <textarea class="m-1" cols="30" rows="10" aria-label="Area to write a new Comments" placeholder="Kommentar" bind:value={comment_content}></textarea><br>
+                <button class="m-1" type="submit" on:submit={publishComment}>Kommentar veröffentlichen</button><br>
+                <span class="font-medium"></span>
             </fieldset>
         </form>
-        <h2>Comments</h2>
         {#each data.post.expand['comments(post)'] as comment}
             <Comment comment={comment}/>
         {/each}
     </section>
 </main>
+
+<style lang="postcss">
+    textarea {
+        width: 100%;
+        resize: vertical;
+    }
+</style>
