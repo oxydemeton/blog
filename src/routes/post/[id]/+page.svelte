f686a180f8450845e2e45babc66341a4420126e3
M B <mablasteam@gmail.com>
2023-03-21 19:37:10 +0100

    added more post data

--- src/routes/post/[id]/+page.svelte
+++ src/routes/post/[id]/+page.svelte
@@ -1,7 +1,16 @@
 <script>
+	import Post from '../../Post.svelte';
+
+
     export let data;
 </script>
 
 <main>
     <h2>{data.post.title}</h2>
+    <div class="italic" aria-label="Posts author">Author: <a href={'/profile/'+data.post.creator}>{data.post.creatorExtend?.username}</a></div>
+    <div class="italic" aria-label="Post release date"><time datetime={data.post.created}>{new Date(data.post.created).toLocaleDateString('de')}</time></div>
+    <hr>
+    <article>
+        {@html data.post.content}
+    </article>
 </main>
