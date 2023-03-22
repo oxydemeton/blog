76e6949af8407af11c11ca9862fe352d3963e403
M B <mablasteam@gmail.com>
2023-03-22 18:36:47 +0100

    added changelogs

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -64,6 +64,7 @@
 {/key}
 
 <footer>
+    <div>Changelogs: <a href="/changes">Liste</a></div>
     <div>Email: <a href="mailto:contact@mabla.name">contact@mabla.name</a></div>
     <div>Stable Blog <a href="https://mabla-meinung.blogspot.com/" target="_blank" rel="noopener noreferrer">mabla-meinung.blogspot.com</a></div>
     <div>Source code: <a href="https://github.com/oxydemeton/blog" target="_blank" rel="noopener noreferrer">@Github</a></div>
