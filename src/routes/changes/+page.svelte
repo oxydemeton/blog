2a2c2312d8359aa6b6a80bbd57ff68c648bcf53b
M B <mablasteam@gmail.com>
2023-03-22 18:38:44 +0100

    added latest change

--- src/routes/changes/+page.svelte
+++ src/routes/changes/+page.svelte
@@ -6,6 +6,9 @@
     <article>
         <ul>
             <li>
+                <time datetime="2023-03-22" >22.03.2023</time>: <i>Disign Fix:</i> Posts auf der Startseite haben einen erhöhten Abstand um die Navigation zu erleichtern.
+            </li>
+            <li>
                 <time datetime="2023-03-22" >22.03.2023</time>: <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
             </li>
             <li>
