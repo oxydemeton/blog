c8f9c6cf49e18c15ec7cefecbd1f33d8835791c4
M B <mablasteam@gmail.com>
2023-03-22 18:36:06 +0100

    added changelogs

---
+++ src/routes/changes/+page.svelte
@@ -0,0 +1,23 @@
+<main>
+    <h2>Änderungen</h2>
+    <p>
+        Hier gibt es alles was sich am Blog verändert hat.
+    </p>
+    <article>
+        <ul>
+            <li>
+                <time datetime="2023-03-22" >22.03.2023</time>: <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
+            </li>
+            <li>
+                <time datetime="2023-03-22" >22.03.2023</time>: <i>Hotfix:</i> Posts ohne Kommentar werden wieder Fehler frei angezeigt. 
+            </li>
+        </ul>
+    </article>
+</main>
+
+<style>
+    h6 {
+        display: inline;
+        font-weight: 600;
+    }
+</style>
