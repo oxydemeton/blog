c51f34e824b11fe01d27df0626f63a144bcacce7
M B <mablasteam@gmail.com>
2023-03-24 20:06:20 +0100

    release 0.1.2

--- src/routes/changes/+page.svelte
+++ src/routes/changes/+page.svelte
@@ -6,13 +6,32 @@
     <article>
         <ul>
             <li>
-                <time datetime="2023-03-22" >22.03.2023</time>: <i>Disign Fix:</i> Posts auf der Startseite haben einen erhöhten Abstand um die Navigation zu erleichtern.
-            </li>
+                <h5>v.0.1.2</h5>
+                <ul>
+                    <li>
+                        <time datetime="2023-03-24" >24.03.2023</time>:
+                        <h6>Feature:</h6>
+                        Passwörter können im <a href="/login">login bereich</a> zurückgesetzt werden.
+                    </li>
+                    <li>
+                        <time datetime="2023-03-24" >24.03.2023</time>:
+                        <h6>Feature:</h6>
+                        Das eigene <a href="/profile">Profil (unter /profile)</a> zeigt nun auch die eigenen Kommentare.
+                    </li>
+                </ul>
             <li>
-                <time datetime="2023-03-22" >22.03.2023</time>: <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
-            </li>
-            <li>
-                <time datetime="2023-03-22" >22.03.2023</time>: <i>Hotfix:</i> Posts ohne Kommentar werden wieder Fehler frei angezeigt. 
+                <h5>Vor 0.1.2</h5>
+                <ul>
+                    <li>
+                        <time datetime="2023-03-22" >22.03.2023</time>: <i>Disign Fix:</i> Posts auf der Startseite haben einen erhöhten Abstand um die Navigation zu erleichtern.
+                    </li>
+                    <li>
+                        <time datetime="2023-03-22" >22.03.2023</time>: <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
+                    </li>
+                    <li>
+                        <time datetime="2023-03-22" >22.03.2023</time>: <i>Hotfix:</i> Posts ohne Kommentar werden wieder Fehler frei angezeigt. 
+                    </li>
+                </ul>
             </li>
         </ul>
     </article>
