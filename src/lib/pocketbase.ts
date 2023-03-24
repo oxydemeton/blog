9b5070a6eda3284706394b7131b2814d286499d0
M B <mablasteam@gmail.com>
2023-03-24 18:57:05 +0100

    made current user contains more information

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -3,6 +3,11 @@ import { writable } from "svelte/store"
 
 export const pb = new PocketBase('https://blog.mabla.name/pb');
 export const currentUser = writable(pb.authStore.model)
-pb.authStore.onChange(auth=> {    
-    currentUser.set(pb.authStore.model) 
+pb.authStore.onChange(async auth=> {
+    if (!pb.authStore.isValid) {
+        return;
+    }
+    if (pb.authStore.model) {
+       currentUser.set(await pb.collection('users').getOne(pb.authStore.model.id));
+    }
 })
