feb7ca2f085024844d0adfa2997cc1a2e89fdfa5
M B <mablasteam@gmail.com>
2023-03-24 19:56:27 +0100

    added password reset

--- src/routes/login/LogInEmail.svelte
+++ src/routes/login/LogInEmail.svelte
@@ -1,5 +1,5 @@
-<script>
-    import {currentUser, pb} from "../../lib/pocketbase"
+<script lang="ts">
+    import {currentUser, pb} from "$lib/pocketbase"
     async function login() {
         pb.collection('users').authWithPassword(email, password)
             .catch(()=>{
@@ -15,6 +15,10 @@
                 }
             })
     }
+    let reset_email = ""
+    function resetPassword() {
+        pb.collection('users').requestPasswordReset(reset_email);
+    }
     let email = ""
     let password = ""
     let detail_message = ""
@@ -27,5 +31,12 @@
         <input type="password" id="login-email-password" placeholder="Password" minlength="4" bind:value={password} aria-label="Password">
         <input type="submit" value="Login">
         <div>{detail_message}</div>
+    </fieldset>
+</form>
+<form on:submit={resetPassword}>
+    <fieldset>
+        <legend>Reset Password</legend>
+        <input type="email" name="reset_email" id="login-email-reset-mail" placeholder="Email address" bind:value={reset_email} aria-label="Email of account to send reset mail.">
+        <input type="submit" value="Reset Password">
     </fieldset>
 </form>
