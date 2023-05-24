b12d956571a08fdb2af79dd8653a0947ad4b776a
M B <mablasteam@gmail.com>
2023-05-24 17:31:06 +0200

    ready for version 0.1.4

--- src/routes/changes/+page.svelte
+++ src/routes/changes/+page.svelte
@@ -7,16 +7,31 @@
         <ul>
             <li>
                 <h5>
-                    v0.1.3
+                    v0.1.4
                 </h5>
+                <time datetime="2023-05-24" >24.05.2023</time>:
                 <ul>
                     <li>
-                        <time datetime="2023-05-23" >23.05.2023</time>:
+                        <h6>Feature:</h6>
+                        Das <a href="/archive" data-sveltekit-preload-code="viewport" data-sveltekit-preload-data="hover">Archive</a> ist nun verfügbar.
+                    </li>
+                    <li>
+                        <h6>Feature:</h6>
+                        Viele Links werden nun beim hovern vorgeladen um die Ladezeiten zu verbessern.
+                    </li>
+                </ul>
+            </li>
+            <li>
+                <h5>
+                    v0.1.3
+                </h5>
+                <time datetime="2023-05-23" >23.05.2023</time>:
+                <ul>
+                    <li>
                         <h6>Feature:</h6>
                         Datum der Veröffentlichung von Posts wird auf der Homepage angezeigt.
                     </li>
                     <li>
-                        <time datetime="2023-05-23" >23.05.2023</time>:
                         <h6>Feature:</h6>
                         Datum der letzten Änderung wird auf der Seite eines Posts angegeben.
                     </li>
@@ -24,29 +39,29 @@
             </li>
             <li>
                 <h5>v.0.1.2</h5>
+                <time datetime="2023-03-24" >24.03.2023</time>:
                 <ul>
                     <li>
-                        <time datetime="2023-03-24" >24.03.2023</time>:
                         <h6>Feature:</h6>
-                        Passwörter können im <a href="/login">login bereich</a> zurückgesetzt werden.
+                        Passwörter können im <a href="/login" data-sveltekit-preload-code="hover">login bereich</a> zurückgesetzt werden.
                     </li>
                     <li>
-                        <time datetime="2023-03-24" >24.03.2023</time>:
                         <h6>Feature:</h6>
-                        Das eigene <a href="/profile">Profil (unter /profile)</a> zeigt nun auch die eigenen Kommentare.
+                        Das eigene <a href="/profile" data-sveltekit-preload-code="hover" data-sveltekit-preload-data="tap">Profil (unter /profile)</a> zeigt nun auch die eigenen Kommentare.
                     </li>
                 </ul>
             <li>
                 <h5>Vor 0.1.2</h5>
+                <time datetime="2023-03-22" >22.03.2023</time>:
                 <ul>
                     <li>
-                        <time datetime="2023-03-22" >22.03.2023</time>: <i>Disign Fix:</i> Posts auf der Startseite haben einen erhöhten Abstand um die Navigation zu erleichtern.
+                         <i>Disign Fix:</i> Posts auf der Startseite haben einen erhöhten Abstand um die Navigation zu erleichtern.
                     </li>
                     <li>
-                        <time datetime="2023-03-22" >22.03.2023</time>: <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
+                        <h6>Feature:</h6> Die Changelogs wurden hinzugefügt.
                     </li>
                     <li>
-                        <time datetime="2023-03-22" >22.03.2023</time>: <i>Hotfix:</i> Posts ohne Kommentar werden wieder Fehler frei angezeigt. 
+                        <i>Hotfix:</i> Posts ohne Kommentar werden wieder Fehler frei angezeigt. 
                     </li>
                 </ul>
             </li>
