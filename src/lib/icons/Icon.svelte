7a02ab7ac002b7c4e7720ce2d987ccff21ca0613
M B <mablasteam@gmail.com>
2023-05-25 17:22:03 +0200

    added icon components

---
+++ src/lib/icons/Icon.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let src: string
+    export let alt: string
+    export let width: number
+    export let height: number
+</script>
+<img {src} {alt} {width} {height}>
---
+++ src/lib/icons/IconCopy.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let width = 128
+    export let height = 128
+    import Icon from "./Icon.svelte"
+    import Img from "@assets/CopyIcon.png"
+</script>
+<Icon {width} {height} src={Img} alt="Click to copy Icon"/>
