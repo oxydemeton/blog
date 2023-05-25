d3bfbfc9649057bf8b21d1672c3b2e9c2ec7b55b
M B <mablasteam@gmail.com>
2023-05-25 17:44:34 +0200

    button now inheriting styles

--- src/lib/Components/CopyText.svelte
+++ src/lib/Components/CopyText.svelte
@@ -6,5 +6,14 @@
         alert(`Copied "${txt}" to clipboard.`)
     }
 </script>
-
+<style lang="postcss">
+    button {
+        unset: all;
+        text-decoration: inherit;
+        border: unset;
+        color: inherit;
+        background: none;
+        font: inherit;
+    }
+</style>
 <button on:click={copy} aria-label="Click to Copy">{txt}<IconCopy height={16} width={16}/></button>
