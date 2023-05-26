78479b6667a0144dcd749e277ca4d2325f1dbe3e
M B <mablasteam@gmail.com>
2023-05-26 22:28:42 +0200

    removed logs

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -2,7 +2,6 @@ import { pb } from '$lib/pocketbase.js'
 
 export async function load({cookies}) {
     const usr = cookies.get('user')
-    console.log("User: " + usr);
     
     return {
         usr: usr
--- src/routes/login/oauth/google/+page.server.ts
+++ src/routes/login/oauth/google/+page.server.ts
@@ -4,7 +4,6 @@ import { redirect } from '@sveltejs/kit';
 
 
 export const load = async ({ url, cookies, }) => {
-    console.log('cookies back home',cookies.get('state'))
     //console.log(url.searchParams);
     const redirectURL = `${url.origin}/login/oauth/google`;
     const expectedState = cookies.get('state');
@@ -23,37 +22,30 @@ export const load = async ({ url, cookies, }) => {
 
     
     if (!authMethods?.authProviders) {
-        console.log('No Auth Providers');
         throw redirect(303, '/');
     }
     const provider = authMethods.authProviders[0];
     if (!provider) {
-        console.log('Provider Not Found');
         throw redirect(303, '/');
     }
 
     if (expectedState !== state) {
-        console.log('Returned State Does not Match Expected', expectedState, state);
         throw redirect(303, '/');
     }
 
     try {
-        console.log("expect verefier: " + expectedVerifier)
         await pb.collection('users')
             .authWithOAuth2Code(provider.name, code!, expectedVerifier! , redirectURL);
         
     } catch (err) {
         console.log('Error logging in with OAuth2 user: ', err);
     }
-    console.log(pb.authStore.model?.id);
     const id = pb.authStore.model?.id;
     if (id) {
         console.log("User identified as: " + id);
         pb.autoCancellation(false);
         const usr = await pb.collection('users').getOne(id, {expand: 'comments(creator).post'}) as unknown as User
-        console.log(usr);
-        pb.autoCancellation(true);
-        currentUser.set(usr);    
+        pb.autoCancellation(true);   
         cookies.set('user', pb.authStore.exportToCookie(), { path: '/' });
     }
     throw redirect(303, '/');
