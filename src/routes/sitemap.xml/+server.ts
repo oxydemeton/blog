4373cad0fa03dbdd1610c8f3a710e730d85b16d6
M B <mablasteam@gmail.com>
2023-06-09 21:21:06 +0200

    fixed sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,24 +1,37 @@
 import { error } from "@sveltejs/kit";
 const last_updated = "2021-06-09"
 export const GET = async () => {
-    const latest_post_url = new URL('https://blog.mabla.name/pb/api/collections/posts/records?page=1&perPage=1&sort=created&fields=created')
+    const latest_post_url = new URL('https://blog.mabla.name/pb/api/collections/posts/records')
     latest_post_url.searchParams.set('page', '1')
     latest_post_url.searchParams.set('perPage', '1')
     latest_post_url.searchParams.set('sort', 'created')
     latest_post_url.searchParams.set('fields', 'created')
-    const latest_post = await (await fetch(latest_post_url)).json() as PostCreated[]
-    if(latest_post.length < 1) {
-        throw error(404, 'No posts found')
-    }else if (!latest_post[0].hasOwnProperty("created")) {
+    let latest_post = undefined
+    console.log(latest_post_url.href);
+    
+    try {
+        const latest_post_raw  = await fetch(latest_post_url)
+        if (!latest_post_raw.ok) throw error(500, 'DB error. Check:' + latest_post_url.href)
+        latest_post = await latest_post_raw.json() as PostCreated
+    } catch (e) {
+        throw error(500, 'DB error. Check:' + latest_post_url.href)
+    }
+    
+    if(latest_post ==  undefined || latest_post.items ==  undefined) {
+        throw error(500, 'DB error. Check:' + latest_post_url.href)
+    } else if (latest_post.items.length == 0 ) {
+        throw error(500, 'DB error. Check:' + latest_post_url.href)
+    } else if (!latest_post.items[0].created == undefined) {
         throw error(500, 'No created property found')
     }
-    return new Response(`\
-    <?xml version="1.0" encoding="UTF-8"?>\
+    const latest_post_created = latest_post.items[0].created
+    return new Response(
+    `<?xml version="1.0" encoding="UTF-8"?>\
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\
         xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\
         <url>\
             <loc>https://blog.mabla.name/</loc>\
-            <lastmod>${latest_post[0].created}</lastmod>\
+            <lastmod>${latest_post_created}</lastmod>\
             <changefreq>weekly</changefreq>\
         </url>\
         <url>\
@@ -31,11 +44,11 @@ export const GET = async () => {
         </url>\
         <url>\
             <loc>https://blog.mabla.name/projects</loc>\
-            <lastmod>${last_updated}9</lastmod>\
+            <lastmod>${last_updated}</lastmod>\
         </url>\
         <url>\
             <loc>https://blog.mabla.name/archive</loc>\
-            <lastmod>${latest_post[0].created}</lastmod>\
+            <lastmod>${latest_post_created}</lastmod>\
         </url>\
         <url>\
             <loc>https://blog.mabla.name/login</loc>\
@@ -53,9 +66,14 @@ export const GET = async () => {
             <loc>https://blog.mabla.name/changes</loc>\
             <lastmod>${last_updated}</lastmod>\
         </url>\
-    </urlset>\
-    `)
+    </urlset>`, {
+        headers: {
+            'content-type': 'application/xml'
+        }
+    })
 };
 type PostCreated = {
-    created: string
+    items: {
+        created: string
+    }[]
 }
